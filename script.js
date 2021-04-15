1ea8e8ca768f194fd760c4d815e95e3ae8279069
Weston Partin <djwest91@gmail.com>
2021-04-15 17:23:04 -0500

    added scripting for menu

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const grind = document.getElementById("grind");
+
+grind.playbackRate = 0.75;
+
+("use strict");
+
+// Let's get our DOM items
+const showcase = document.querySelector(".showcase");
+const bars = document.getElementById("bars");
+const close = document.getElementById("close");
+
+// These are our two functions
+function openMenu(e) {
+  e.preventDefault();
+  showcase.classList.add("active");
+  bars.classList.add("hidden");
+  close.classList.remove("hidden");
+}
+function closeMenu(e) {
+  e.preventDefault();
+  showcase.classList.remove("active");
+  close.classList.add("hidden");
+  bars.classList.remove("hidden");
+}
+
+// Here are our event listeners for click & touch :)
+bars.addEventListener("click", openMenu);
+close.addEventListener("click", closeMenu);
+bars.addEventListener("touchend", openMenu);
+close.addEventListener("touchend", closeMenu);
